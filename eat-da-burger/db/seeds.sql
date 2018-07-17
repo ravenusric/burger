cbbf61472bff8198e0a65da353fc62b626814621
ravenusric <richie.roney@gmail.com>
2018-07-16 23:59:17 -0400

    added files

---
+++ eat-da-burger/db/seeds.sql
@@ -0,0 +1,32 @@
+INSERT INTO burgers (
+burger_name,
+devoured
+) VALUES
+(
+"Barth Burger",
+0
+),
+(
+"McDowell Burger",
+0
+),
+(
+"The Big Kahuna Burger",
+0
+);
+(
+"Mooby Burger",
+0
+);
+(
+"The Max Burger",
+0
+);
+(
+"McBurgerTown Burger",
+0
+);
+(
+"Paunch Burger",
+0
+);
